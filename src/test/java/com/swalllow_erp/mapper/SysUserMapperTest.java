34884cc78b2e8c881406d54f1cda1eb942741ff9
swallow333 <3322026873@qq.com>
2026-05-21 18:30:41 +0800

    feat(test): 增加SysUserMapper测试类

---
+++ src/test/java/com/swalllow_erp/mapper/SysUserMapperTest.java
@@ -0,0 +1,18 @@
+package com.swalllow_erp.mapper;
+
+import com.swalllow_erp.SwallowErpApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SwallowErpApplication.class)
+public class SysUserMapperTest {
+    @Autowired  // 自动注入
+    private SysUserMapper sysUserMapper;  // 4. 注入Mapper对象
+
+    @Test // 标记为测试方法
+    public void test() {  // 测试方法
+        // 调用Mapper的方法查询所有用户
+        System.out.println(sysUserMapper.selectList(null));
+    }
+}
